f3b9f78fee8e5613dd6600f76cfed5160f693366
areva <areva@LAPTOP-EAM40Q36.attlocal.net>
2022-09-28 15:04:49 -0500

    giving 5% extra offer for iphone 13 pro

--- src/main/java/com/example/demo/controller/offers.java
+++ src/main/java/com/example/demo/controller/offers.java
@@ -5,7 +5,7 @@ public class offers {
 	public static void main(String[] args) {
 		System.out.println("10% discount on laptops");
 		System.out.println("Offers on Iphone 13 Pro 20% discount if you buy today");
-
+       System.out.println("take an extra 5% off on iphone 13 pro today");
 	}
 
 }
