2850b2961165bdff80c98f6609a410431960fa6d
areva <areva@LAPTOP-EAM40Q36.attlocal.net>
2022-09-28 14:57:53 -0500

    updating offer iphone 20% discount

--- src/main/java/com/example/demo/controller/offers.java
+++ src/main/java/com/example/demo/controller/offers.java
@@ -4,6 +4,7 @@ public class offers {
 
 	public static void main(String[] args) {
 		System.out.println("10% discount on laptops");
+		System.out.println("Offers on Iphone 13 Pro 20% discount if you buy today");
 
 	}
 
